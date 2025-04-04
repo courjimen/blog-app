79d8542ebef39dc5d98a571e86c3c73f372eda95
courjimen <coureyfrance@gmail.com>
2025-04-03 21:58:32 -0400

    created folders for backend server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import cors from 'cors'
+import pool from './db.js'
+
 const app = express();
 const port = 3000
 
+app.use(cors())
 app.use(express.json());
 
 app.listen(port, () => {
